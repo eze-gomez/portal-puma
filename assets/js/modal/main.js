4cf4f5f34addff57dc154f5addfd05103c3a2ed1
eze-gomez <eze.gomez@gmail.com>
2024-03-04 13:30:34 -0300

    lunes nuevo 2

--- assets/js/modal/main.js
+++ assets/js/modal/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
+// loads when document is ready ORIGINAL
 
-	// 
+if (document.cookie.indexOf('modal_shown=') >= 0) {
+	//do nothing if modal_shown cookie is present
+   } else {
 	
-	$('#exampleModalCenter').modal('show');
+$('#exampleModalCenter').modal('show'); // show modal pop up ORIGINAL
 
-	
+document.cookie = 'modal_shown=seen'; //set cookie modal_shown
+  //cookie will expire when browser is closed
+}	
 
 
 	
